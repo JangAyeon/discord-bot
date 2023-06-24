94b0c0307ae70fdb4b85c4b71b7fd28c8fd7624a
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-06-24 22:34:07 +0900

    add: firebase setup

---
+++ src/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from "firebase/firestore/lite"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBCocD4F3ptzm-pm36y-8uuGOudQSkxcOg",
+  authDomain: "help-discord-bot.firebaseapp.com",
+  projectId: "help-discord-bot",
+  storageBucket: "help-discord-bot.appspot.com",
+  messagingSenderId: "1082685696461",
+  appId: "1:1082685696461:web:83c243579acc2076adfc84",
+  measurementId: "G-BSVWJTK9EN",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
